da71d036825ae896e6a8e743bf860d9a547f1916
Elsa <70358571+elsarr@users.noreply.github.com>
2023-11-16 10:27:35 -0500

    Add files via upload

---
+++ CryptoCoins.java
@@ -0,0 +1,53 @@
+//--------------------------------------
+//Assignment 1 question 1
+//Written by: Elsa Rufenacht 40235345
+//For COMP248 Section EC - Winter 2023
+//--------------------------------------
+
+//This program determines the change to be given back in crypto coins of the forthcoming millennium 
+
+import java.util.Scanner;
+
+public class CryptoCoins 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to the Crypto Program:");
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		Scanner myKeyboard = new Scanner(System.in);
+		System.out.print("Enter the price of the ticket in Cryptom (at most 7500):");
+		int cryptom = myKeyboard.nextInt(); //the value entered by the user is the value of the integer cryptom
+		System.out.println();
+		
+		final int pesico = 7500; //1 pesico is 7500 cryptom
+		int coinsBack = pesico - cryptom; //number of coins to give back in cryptom
+		int bitom = coinsBack/500; //1 bitom is 500 cryptom 
+		int restBitom = coinsBack%500;//remaining coins in ditom
+		int ditom = restBitom/200; //number of ditoms, 1 ditom is 200 cryptom
+		int restDitom = restBitom%200;//remaining coins in zitom
+		int zitom = restDitom/50; //number of zitoms, 1 zitom is 50 cryptom
+		int restZitom = restDitom%50;//remaining coins in cryptom
+		int newCryptom = restZitom; //remaining coins in cryptom renamed  
+				
+		
+		System.out.println("You bought a ticket for " + cryptom + " Cryptom. and gave me a Pesico, so your change is");
+		System.out.println(bitom + " Bitom,");
+		System.out.println(ditom + " Ditom,");
+		System.out.println(zitom + " Zitom, and");
+		System.out.println(newCryptom + " Cryptom.");
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("Thank you for using my bespoke Crypto Change Program!");
+		System.out.println();
+		System.out.print("Enjoy the Game!");
+		
+		myKeyboard.close();//close scanner 
+		
+
+	}
+
+}
---
+++ TicketCalculator.java
@@ -0,0 +1,88 @@
+//Assignment 1 question 2 
+//Written by: Elsa Rufenacht 40235345
+//For COMP 248 Section EC - Winter 2023
+
+//This program calculates the number of movie ticekts someone can buy with a prepaid giftcard
+
+import java.util.Scanner;
+
+public class TicketCalculator 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to the Simple Cinema Tickets Invoice Program:");
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		final double price = 6.99; //set the constant price of a ticket 
+		
+		Scanner keyIn = new Scanner(System.in); //create a scanner
+		System.out.print("Enter the prepaid giftcard retailer/bank name: ");
+		String name = keyIn.nextLine(); //scan everything that was written previously 
+		String nameUpper = name.toUpperCase();
+		System.out.println("You will be using " + nameUpper + " gift card for your ticket(s) purchase.");
+		
+		System.out.print("Enter the amount on the gift card (amount should be greater than "+price+"):");//prompting the user to enter an amount bigger than the ticket price 
+		double amount = keyIn.nextDouble();
+		System.out.println("There is a fund of $" + amount + " on your prepaid " + nameUpper + " gift card.");
+		
+		int tickets = (int)(amount/price);//convert double into integer for the number of tickets 
+		float balance = (float)(amount%price); //convert double to float because float is smaller and will display only 2 decimals 
+		System.out.print("Using your " + nameUpper + " gift card you can purchase " + tickets + " tickets and have a balance of $");
+		System.out.printf("%.2f", balance);//make balance print with 2 digits after decimal
+		System.out.println(" on the gift card.");
+		
+		System.out.print("Enter the number of tickets you want to purchase (less than or equal to " +tickets+"):");
+		int number = keyIn.nextInt();
+		double purchase = number*price;
+		System.out.println("The purchase of " + number + " ticket(s) costs $" + purchase + "."); 
+		
+		float newBalance = (float)(amount - purchase);
+		System.out.println("The balance left on your " + nameUpper + " gift card is $" + newBalance + ".");
+		System.out.println();
+		
+		System.out.println("Please enter your purchase info:");
+		System.out.println();
+		
+		System.out.print("Day of purcahse (between 1 and 31): ");
+		int day = keyIn.nextInt();
+		
+		System.out.print("Month of purcahse (between 1 and 12): ");
+		int month = keyIn.nextInt();
+		
+		System.out.print("Year of purcahse (between 2023 and 2028): ");
+		int year = keyIn.nextInt();
+		
+		System.out.println("_________________________________________________________________________________");
+		System.out.println("                         Cinema Concordia      "+ day+ "/" + month + "/" + year);
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println("_________________________________________________________________________________");
+		
+		System.out.println();
+		System.out.println(number + " Tickets                                                                 $" + purchase);
+		System.out.println("$"+purchase+ " was redeemed from "+nameUpper+" prepayed gift card.");
+		System.out.println(nameUpper+ " gift card balance                                           $"+newBalance);
+		System.out.println("_________________________________________________________________________________");
+		System.out.println("Invoice generated successfully.");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println("Thank you for usig my bespoke Cinema Tickets Invoice Program!");
+		
+		
+		keyIn.close();//close scanner
+	
+				
+		
+		
+
+	
+	}
+
+	
+
+
+}
